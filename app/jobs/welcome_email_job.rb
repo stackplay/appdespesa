487cf1a46174eb0ee97b956f5947ac9677f90f6a
Gabriel Santos <reinaldsantos312@gmail.com>
2026-05-07 20:41:34 +0100

    fix: use find_by in WelcomeEmailJob to avoid RecordNotFound

--- app/jobs/welcome_email_job.rb
+++ app/jobs/welcome_email_job.rb
@@ -2,7 +2,8 @@ class WelcomeEmailJob < ApplicationJob
   queue_as :default
 
   def perform(user_id)
-    user = User.find(user_id)
+    user = User.find_by(id: user_id)
+    return unless user
     ExpenseMailer.welcome_email(user).deliver_now
   end
 end
