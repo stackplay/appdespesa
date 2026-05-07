8feb477660dfc55661f0b424db839e27ea68a8e4
Gabriel Santos <reinaldsantos312@gmail.com>
2026-05-07 20:24:13 +0100

    fix: increase redis timeout for Upstash

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,14 @@
-if ENV["REDIS_URL"].present?
-  Sidekiq.configure_server do |config|
-    config.redis = { url: ENV["REDIS_URL"] }
-  end
+redis_config = {
+  url: ENV.fetch("REDIS_URL", "redis://localhost:6379/0"),
+  timeout: 30,
+  read_timeout: 30,
+  write_timeout: 30
+}
 
-  Sidekiq.configure_client do |config|
-    config.redis = { url: ENV["REDIS_URL"] }
-  end
+Sidekiq.configure_server do |config|
+  config.redis = redis_config
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = redis_config
 end
